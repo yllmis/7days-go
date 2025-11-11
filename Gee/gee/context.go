364a09a25d324e15a6a7e319566fed5fe798ab19
yllmis <yllmis@YJS.localdomain>
2025-11-11 11:21:49 +0800

    Gee中设计Context

---
+++ Gee/gee/context.go
@@ -0,0 +1,46 @@
+type H map[string]interface{}
+
+type Context struct {
+	Writer ResponseWriter
+	Req    *Request
+
+	Path   string
+	Method string
+
+	StatusCode int
+}
+
+func newContext(w ResponseWriter, req *Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    req,
+
+		Path:   req.URL.Path,
+		Method: req.Method,
+	}
+}
+
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+func (c *Context) JSON(code int)
